3cee283fb9da359074442eaf8b50072915fd3e0f
saja-ahmed <codeidea2024@gmail.com>
2025-08-21 07:06:12 +0300

    update firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // ضع هنا بيانات Firebase الخاصة بك
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyAO9NfsAKt40n4E3Mipq8IdOB9pvsxJUJI",
+  authDomain: "dana-socks.firebaseapp.com",
+  projectId: "dana-socks",
+  storageBucket: "dana-socks.appspot.com",  // ✅ التعديل هنا
+  messagingSenderId: "967973245549",
+  appId: "1:967973245549:web:7ac4a30b74822177c4e564",
+  measurementId: "G-PX23Z0V0QV"
 };
 
 const app = initializeApp(firebaseConfig);
